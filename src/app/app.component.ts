5103e25abddbfa809c861298701d61f361ca7f8b
Matt Lewis <hello@mattlewis.me>
2022-09-07 12:18:31 +0100

    Create reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-localize-bug';
+  title = $localize`:@@login-form-components--lock-timeout-sec:Account locked. Try again in ${this.pluralizeTimeout(4, $localize`:@@login-form-components--lock-timeout-sec-single:second`,$localize`:@@login-form-components--lock-timeout-sec-plural:seconds`)}:accountLockTimeoutSec:.`;
+
+  pluralizeTimeout(timeout: number, single: string, plural: string): string {
+    return timeout + ' ' + (timeout === 1 ? single : plural);
+  }
 }
